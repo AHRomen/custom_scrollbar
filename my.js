88b1d6a468905dfc5abaaad2d83ed3edebe5cd4d
Arman Hossain <romen.dm777@gmail.com>
2024-11-03 19:54:09 +0600

    moving is done need to optimize

--- my.js
+++ my.js
@@ -1,73 +1,32 @@
-// let parent = document.querySelector(".parent");
-// let child = document.querySelector(".child");
-// let scroll_bar = document.querySelector(".scrollbar");
-// let s_thumb = document.querySelector(".s-thumb");
-// let p_w = parent.getBoundingClientRect().width;
-// let c_w = child.getBoundingClientRect().width;
-// let s_thumb_width =(p_w / (c_w - p_w)) *100;
-// s_thumb.style.width = s_thumb_width + "%";
-// let isDragging = true;
-// let startX;
-// function draggingElement(e){
-//     let isDragging = true;
-//     s_thumb.addEventListener('mousemove', (e)=>{
-//         startX =100 - s_thumb_width;
-//         if(isDragging){
-               
-//         }
-//     });
-//     s_thumb.addEventListener("mouseup", (e)=>{
-//         isDragging = false;
-//     })
-// }
-// s_thumb.addEventListener("mousedown", draggingElement);
-
 let parent = document.querySelector(".parent");
 let child = document.querySelector(".child");
 let scroll_bar = document.querySelector(".scrollbar");
 let s_thumb = document.querySelector(".s-thumb");
-
 let p_w = parent.getBoundingClientRect().width;
 let c_w = child.getBoundingClientRect().width;
-let s_thumb_width = (p_w / (c_w - p_w)) * 100;
-
+// let s_thumb_width =(p_w / (c_w - p_w)) *100;
+let s_thumb_width =(p_w / c_w) *100;
 s_thumb.style.width = s_thumb_width + "%";
-
-let isDragging = false;
-let startX;
-
-function draggingElement(e) {
-    isDragging = true;
-    startX = e.clientX; // Store initial mouse position
-
-    // Update position while dragging
-    const onMouseMove = (e) => {
-        if (isDragging) {
-            // Calculate new thumb position
-            let newLeft = e.clientX - startX + s_thumb.offsetLeft;
-            const maxLeft = p_w - (s_thumb_width / 100 * p_w); // Max position of the thumb
-
-            // Constrain thumb position
-            if (newLeft < 0) newLeft = 0;
-            if (newLeft > maxLeft) newLeft = maxLeft;
-
-            s_thumb.style.left = newLeft + "px"; // Update thumb position
+let isDragging = true;
+function draggingElement(e){
+    let isDragging = true;
+    scroll_bar.addEventListener('mousemove', (e)=>{
+        if(isDragging){
+            let thumb_pos = (((e.clientX - parent.getBoundingClientRect().left) / p_w) * 100);
             
-            // Update scroll position
-            const scrollPercentage = newLeft / maxLeft;
-            parent.scrollLeft = scrollPercentage * (c_w - p_w);
+            total_distance = thumb_pos + s_thumb_width;
+
+            
+            
+            if(total_distance <= 100){               
+                s_thumb.style.left = thumb_pos + "%";
+                // child.style.left = '-' + (total_distance + s_thumb_width) + "%";
+                child.style.left = '-' + ((c_w - p_w) - ((c_w / 100) * (100 - total_distance))) + "px";
+            }
         }
-    };
-
-    // Stop dragging
-    const onMouseUp = () => {
+    });
+    s_thumb.addEventListener("mouseup", (e)=>{
         isDragging = false;
-        document.removeEventListener("mousemove", onMouseMove);
-        document.removeEventListener("mouseup", onMouseUp);
-    };
-
-    document.addEventListener("mousemove", onMouseMove);
-    document.addEventListener("mouseup", onMouseUp);
+    })
 }
-
 s_thumb.addEventListener("mousedown", draggingElement);
